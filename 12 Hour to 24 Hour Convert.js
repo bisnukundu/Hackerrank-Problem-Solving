614bfbb1fc8b0207ebe7397da3dbc4090231f29e
Bisnu kundu <bisnukundu@gmail.com>
2022-01-06 02:02:48 +0600

    add Comment for 12 hour to 24 hour convert

--- 12 Hour to 24 Hour Convert.js
+++ 12 Hour to 24 Hour Convert.js
@@ -1,3 +1,6 @@
+//you simply add 12 to the the number of hours that have passed. For example: 3:00 p.m. is 3 hours after 12:00 p.m. (midday). 3 p.m. + 12 hours = 15:00. 8:30 p.m. is 8 hours 30 minutes after 12:00 p.m. 8 hours 30 minutes + 12 hours = 20:30.
+
+
 // Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
 
 let time = '12:45:54PM';
@@ -15,7 +18,7 @@ function TimeConvert(s) {
 
     let hour = timeSplite[0][0] === '0' ? timeSplite[0][1] : timeSplite[0];
 
-    return `${hour !== '12' ? Number(hour) + 12 :hour}:${timeSplite[1]}:${second[0]}`
+    return `${hour !== '12' ? Number(hour) + 12 : hour}:${timeSplite[1]}:${second[0]}`
 
 }
 
