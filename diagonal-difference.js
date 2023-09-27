27f9b89f0889c92316a23d64f735178187e129b4
bisnukundu <bisnukundu@gmail.com>
2023-09-27 16:48:52 +0600

    grading-students problem solve

--- diagonal-difference.js
+++ diagonal-difference.js
@@ -4,11 +4,44 @@
  * The function is expected to return an INTEGER.
  * The function accepts 2D_INTEGER_ARRAY arr as parameter.
  */
+
 let arr = [
     [11, 2, 4],
     [4, 5, 6],
-    [10, 8, -12]
+    [10, 8, 80]
 ];
+const matrix = [
+    [6, 6, 7, -10, 9, -3, 8, 9, -1],
+    [9, 7, -10, 6, 4, 1, 6, 1, 1],
+    [-1, -2, 4, -6, 1, -4, -6, 3, 9],
+    [-8, 7, 6, -1, -6, -6, 6, -7, 2],
+    [-10, -4, 9, 1, -7, 8, -5, 3, -5],
+    [-8, -3, -4, 2, -3, 7, -5, 1, -5],
+    [-2, -7, -4, 8, 3, -1, 8, 2, 3],
+    [-3, 4, 6, -7, -7, -8, -3, 9, -6],
+    [-2, 0, 5, 4, 4, 4, -3, 3, 0]
+];
+
+//left = 80
+//right = 90
+
+
+
+// console.log(matrix[0][0])
+
+for(let i = 0; i < matrix.length; i++){
+    // console.log(matrix[i][i])
+    console.log(matrix[i].length)
+}
+
+
+
+
+
+
+
+
+
 
 
 function diagonalDifference(arr) {
@@ -18,13 +51,13 @@ function diagonalDifference(arr) {
     for (let i = 0; i < arr.length; i++) {
         leftToRight += arr[i][i]
         rightToLeft += arr[i][(arr.length - 1) - i]
-
-
     }
 
     let result = rightToLeft - leftToRight;
-    console.log(result)
+
     return Math.abs(result);
+
+    return result;
 }
 
-diagonalDifference(arr)
+diagonalDifference(matrix)
---
+++ grading-students.js
@@ -0,0 +1,75 @@
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+let grades = [73, 67, 38, 33,]
+let arr = [23,
+    80,
+    96,
+    18,
+    73,
+    78,
+    60,
+    60,
+    15,
+    45,
+    15,
+    10,
+    5,
+    46,
+    87,
+    33,
+    60,
+    14,
+    71,
+    65,]
+function gradingStudents(grades) {
+    // console.log(grades)
+
+    let result = [];
+
+    grades.forEach(grade => {
+        let makeRoundGrade = Number(String(grade)[0]) * 10 + 5;
+        let isZero = Number(String(grade)[1]) == 0;
+        let isFive = Number(String(grade)[1]) == 5;
+        let roundGrade = 0;
+        let minimunGrade = 38
+
+        if (grade > makeRoundGrade && grade >= minimunGrade && !isZero && !isFive) {
+
+            roundGrade = Number(String(grade)[0]) * 10 + 10
+
+            if ((roundGrade - grade) < 3) {
+                roundGrade = Number(String(grade)[0]) * 10 + 10
+            } else {
+                roundGrade = grade
+            }
+
+        } else if (grade < makeRoundGrade && grade >= minimunGrade && !isZero && !isFive) {
+            roundGrade = Number(String(grade)[0]) * 10 + 5
+
+            if ((roundGrade - grade) < 3) {
+                roundGrade = Number(String(grade)[0]) * 10 + 5
+            } else {
+                roundGrade = grade
+            }
+
+        } else if (grade < 38 || isZero || isFive) {
+            roundGrade = grade
+        }
+
+        result.push(roundGrade);
+        console.log(`Orginal = ${grade} | Round = ${roundGrade}`);
+
+        console.log("-----------------------")
+
+    });
+
+    console.log('Result: ' + result)
+    return result;
+}
+
+gradingStudents(arr);
